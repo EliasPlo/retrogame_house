c07466e6ffa83355cdcf2f93f9e4ffef794e8b05
Elias Pölönen <elias.polonen06@outlook.com>
2024-09-30 14:30:01 +0300

    1.7

--- assets/js/game.js
+++ assets/js/game.js
@@ -5,21 +5,29 @@ async function fetchGameData() {
         const games = data.games;
 
         // Get the game ID from the URL
+        document.addEventListener('DOMContentLoaded', function () {
+
+        })
         const urlParams = new URLSearchParams(window.location.search);
         const gameId = urlParams.get('id');
 
-        // Find the game with the matching ID
-        const game = games.find(g => g.ID == gameId);
+/*fetch('/data/games.json')
+.then(response => response.json())
+.then(data => {
 
+})*/
+        const game = games.find(g => g.ID == gameId);
         if (game) {
             displayGameDetails(game);
+            gameResults = game.hall_of_fame;
+            displayResults(gameResults);
         } else {
             console.error('Game not found');
         }
     } catch (error) {
         console.error('Error fetching game data:', error);
     }
-}
+};
 
 function displayGameDetails(game) {
     const gameTitle = document.getElementById('gameTitle');
